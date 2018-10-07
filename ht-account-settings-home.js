20ec426dd87a04ef4685a21da0c87df5ed7b50c3
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-10-07 19:22:59 +0300

    Update ht-account-settings-home.js
    add avtar hover and link to user profile

--- ht-account-settings-home.js
+++ ht-account-settings-home.js
@@ -39,18 +39,51 @@ class HTAccountSettingsHome extends LitElement {
     
         #avatar {
             grid-column: 1 / 4;
+            display:flex;
+            flex-direction: column;
+            align-items: center;
         }    
     
         #avatar > a {
             display: block;
+            position: relative;
             margin: auto;
             width: 128px;
             height: 128px;
             border-radius: 50%;
             overflow: hidden;
         }
+
+        #avatar a div {
+            display:none;
+            position: absolute;
+            top:0;
+            right:0;
+            bottom:0;
+            left:0;
+            background-color: hsla(0, 0%, 0%, 0.5);
+        }
+
+        #avatar > a:hover div {
+            display:flex;
+            justify-content:center;
+            align-items:center;
+        }
+
+        #avatar > div {
+            margin-top: 16px;
+            font-size: 18px;
+        }
+
+        #avatar iron-icon {
+            margin:0;
+            width: 48px;
+            height: 48px;
+            color: #fff;
+        }
     
         .item {
+            overflow:hidden;
             padding: 24px;
             //box-shadow: 0 3px 3px -2px rgba(0, 0, 0, .2), 0 3px 4px 0 rgba(0, 0, 0, .14), 0 1px 8px 0 rgba(0, 0, 0, .12);
             display: flex;
@@ -121,6 +154,7 @@ class HTAccountSettingsHome extends LitElement {
                 <g id="settings">
                     <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"></path>
                 </g>
+                <g id="photo-camera"><circle cx="12" cy="12" r="3.2"></circle><path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"></path></g>
             </defs>
         </svg>
     </iron-iconset-svg>
@@ -138,7 +172,9 @@ class HTAccountSettingsHome extends LitElement {
       data.avatar.public_id
     }.${data.avatar.format}">
                 </ht-image>
+                <div><iron-icon icon="ht-account-settings-home:photo-camera"></iron-icon></div>
             </a>
+            <div><a href="/user/${data.uid}">${data.displayName}</a></div>
         </div>
         <div id="security" class="item">
             <div class="title">
